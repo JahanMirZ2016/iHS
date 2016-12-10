b7a8ad27b75ce85a3cf388a05d6bb590e2182b44
Unknown <arashqu1ckz@yahoo.com>
2016-12-10 12:55:40 +0330

    Arash: NotifyVC fixed.

--- iHS Swift/NotifyVC.swift
+++ iHS Swift/NotifyVC.swift
@@ -91,18 +91,18 @@ class NotifyVC: UIViewController {
 ///Arash : Extensions for UITablview Delegate and Datasource
 extension NotifyVC: UITableViewDataSource {
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//        return (notifyArray?.count)!
-        return 1
+        //        return (notifyArray?.count)!
+        return (DBManager.getLastNotifies()?.count)!
     }
     
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = CellNotify(frame: CGRect(x: 0, y: 0, width: WIDTHPHONE, height: 0))
-//        cell.textText = notifyArray![indexPath.row].notifyText
-//        cell.titleText = notifyArray![indexPath.row].notifyTitle
-//        cell.context = self
-//        cell.notifyModel = notifyArray![indexPath.row]
-//        cell.row = indexPath.row
+                cell.textText = notifyArray![indexPath.row].notifyText
+                cell.titleText = notifyArray![indexPath.row].notifyTitle
+                cell.context = self
+                cell.notifyModel = notifyArray![indexPath.row]
+                cell.row = indexPath.row
         
         
         cell.layoutIfNeeded()
@@ -135,7 +135,7 @@ extension NotifyVC: UITableViewDelegate {
             let allNotifyCount = DBManager.getAllNotifies()?.count
             DBManager.deleteNotify(NotifyID: allNotifyCount! - indexPath.row)
             tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Right)
-//            self.notifyArray?.removeAtIndex(indexPath.row)
+            self.notifyArray?.removeAtIndex(indexPath.row)
             
             
         }
